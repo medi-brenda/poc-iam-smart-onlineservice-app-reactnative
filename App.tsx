13ee840f2f9f095bc3753457eadf48ed8e56cb73
unknown <brenda_hon@mediconcen.com>
2022-06-05 11:38:51 +0800

    comment

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ const easAppURL = "exp+poc-iam-smart-mobile-app-reactnative://expo-development-c
 const expoRedirectURL = "https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-mobile-app-reactnative_expo.html";
 const easRedirectURL = "https://mediconcen.com/poc-iam-smart/poc-iam-smart-html/call-poc-iam-smart-mobile-app-reactnative_eas.html";
 
+// React Native Linking
 const OpenURLButton = ({ url, children }) => {
   const handlePress = useCallback(async () => {
     // Checking if the link is supported for links with custom URL scheme.
@@ -62,6 +63,7 @@ export default class App extends Component {
     );
   }
 
+  // Expo Linking
   _handleOpenWithExpoLinkingExpoURL = () => {
     ExpoLinking.openURL(expoAppURL);
   };
@@ -70,6 +72,7 @@ export default class App extends Component {
     ExpoLinking.openURL(easAppURL);
   };
 
+  // Expo Web Browser
   _handleOpenWithExpoWebBrowserExpoURL = () => {
     ExpoWebBrowser.openBrowserAsync(expoRedirectURL);
   };
